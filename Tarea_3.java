45ea8012862a45c5e039a3ba536ae208cad1eb46
DavidPeinadoD <davidpeinadodiaz@gmail.com>
2023-02-17 19:01:22 +0100

    Segunda version: Ejercicio 1 y ejercicio 2

--- Tarea_3.java
+++ Tarea_3.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 //Ejercicio 1
+/*
 public class Tarea_3 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
@@ -7,6 +8,22 @@ public class Tarea_3 {
         int temperatura = teclado.nextInt();
         if (temperatura < 2) {
             System.out.println("Riesgo de hielo");
+        }
+    }
+}*/
+public class Tarea_3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce la temperatura");
+        int temperatura = teclado.nextInt();
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        } else if (temperatura >= 2 && temperatura < 15) {
+            System.out.println("Hace frio");
+        } else if (temperatura >= 15 && temperatura < 30) {
+            System.out.println("Buena temperatura");
+        } else if (temperatura >= 30) {
+            System.out.println("Hace calor");
         }
     }
 }
